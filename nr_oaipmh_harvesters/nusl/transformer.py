de5cf4b0ae62a1b8d5803dde89eb053b8f691009
Juraj Trappl <juraj.trappl@proton.me>
2025-01-24 14:15:40 +0100

    remove the check where the bug is

--- nr_oaipmh_harvesters/nusl/transformer.py
+++ nr_oaipmh_harvesters/nusl/transformer.py
@@ -339,9 +339,6 @@ def transform_650_7_subject(md, entry, value):
 
 
 def transform_subject(md, value):
-    if any(v is None for v in value):
-        return
-
     purl = value[3] or ""
     val_url = (
         purl if purl.startswith("http://") or purl.startswith("https://") else None
