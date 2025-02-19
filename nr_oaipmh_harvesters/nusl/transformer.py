d1d0a8c278fd4e3fa182c67f14ccffa50816c4b9
Juraj Trappl <juraj.trappl@proton.me>
2025-02-19 10:42:50 +0100

    add more stuff to funders matching

--- nr_oaipmh_harvesters/nusl/transformer.py
+++ nr_oaipmh_harvesters/nusl/transformer.py
@@ -600,27 +600,81 @@ def transform_996_accessibility(md, entry, value):
 
 @matches("999C1a", "999C1b", paired=True)
 def transform_999C1_funding_reference(md, entry, val):
-    # Handle what happens when project ID is missing.
-    
     project_id, funder = val
     if funder and project_id:
         from invenio_vocabularies.proxies import current_service
         from invenio_access.permissions import system_identity
 
+        def has_close_substring(shorter, longer, max_distance=1):
+            """Check if any substring of longer (of length equal to shorter) has Levenshtein distance <= max_distance from shorter"""
+            shorter_len = len(shorter)
+            for i in range(len(longer) - shorter_len + 1):
+                substring = longer[i:i + shorter_len]
+                if Levenshtein.distance(shorter, substring) <= max_distance:
+                    return True
+            return False
+
         try:
-            escaped_funder = lucene_escape(funder)
-            query_parts = [
-                'title.*:"{}"',
-                'props.acronym:"{}"',
-                'nonpreferredLabels.*:"{}"'
-            ]
-                        
-            query = " OR ".join(query_parts).format(escaped_funder)
+            resp = current_service.search(
+                system_identity, 
+                type="funders", 
+                params={"q": "", "size": 1000}
+            )
+            all_funders = list(resp)
             
-            resp = current_service.search(system_identity, type="funders", params={"q": query})
-            matched_funder = list(resp)[0]
-        except sqlalchemy.exc.NoResultFound:
-            raise KeyError(f"Funder: '{funder}' has not been found")
+            found_funder = None
+            search_funder = funder.lower()
+            for f in all_funders:
+                if funder == f.get('id'):
+                    found_funder = f
+                    break
+                
+                for lang, title in f.get('title', {}).items():
+                    title_lower = title.lower()
+                    # check both directions - is title in search_funder or vice versa
+                    if len(title_lower) <= len(search_funder):
+                        if has_close_substring(title_lower, search_funder):
+                            found_funder = f
+                            break
+                    else:
+                        if has_close_substring(search_funder, title_lower):
+                            found_funder = f
+                            break
+                            
+                if found_funder:
+                    break
+                    
+                acronym = f.get('props', {}).get('acronym', '').lower()
+                if acronym:
+                    if len(acronym) <= len(search_funder):
+                        if has_close_substring(acronym, search_funder):
+                            found_funder = f
+                            break
+                    else:
+                        if has_close_substring(search_funder, acronym):
+                            found_funder = f
+                            break
+                
+                for label in f.get('nonpreferredLabels', []):
+                    for lang, text in label.items():
+                        text_lower = text.lower()
+                        if len(text_lower) <= len(search_funder):
+                            if has_close_substring(text_lower, search_funder):
+                                found_funder = f
+                                break
+                        else:
+                            if has_close_substring(search_funder, text_lower):
+                                found_funder = f
+                                break
+                    if found_funder:
+                        break
+            
+            if not found_funder:
+                raise KeyError(f"Funder: '{funder}' has not been found")
+
+            matched_funder = found_funder
+        except Exception as e:
+            raise KeyError(f"Funder: '{funder}' has not been found") from e
 
         md.setdefault("fundingReferences", []).append(
             make_dict("projectID", project_id, "funder", matched_funder)
